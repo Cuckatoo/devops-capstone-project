92a67b708b633c2fa451675c7b80def27030aa37
Cuckatoo <132697521+Cuckatoo@users.noreply.github.com>
2023-07-05 23:14:17 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -63,6 +63,7 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -83,6 +84,7 @@ def list_accounts():
 
 # ... place you code here to READ an account ...
 
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
     """
@@ -94,7 +96,7 @@ def read_account(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id {account_id} could not be found.")
-    
+      
     return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
@@ -102,6 +104,7 @@ def read_account(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -123,6 +126,7 @@ def update_accounts(account_id):
 ######################################################################
     # DELETE AN ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
